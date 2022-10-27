d99c11af5cfca9def873826151433c61414fcc9d
Feeeedor <fedor20031912@yandex.ru>
2022-10-27 20:54:06 +0300

    case i print

--- prog2.h
+++ prog2.h
@@ -8,12 +8,11 @@ class Snail
 private:
     double a;
     double b;
-
 public:
     Snail(double newA, double newB);
-    
- double GetA();
- double GetB();
+
+    double GetA();
+    double GetB();
     void SetA(double newA);
 
     void SetB(double newB);
@@ -24,23 +23,22 @@ public:
     int Type_of_Snail();
 
     double Square_snail();
-void Radius(double &r1, double &r2, double &r3);
+    void Radius(double &r1, double &r2, double &r3);
     char *decart();
 };
 
-
 int dialog();
 
-bool snail_d_SetA(Snail &z);
-bool snail_d_SetB(Snail &z);
-bool snail_d_GetA(Snail &z);
-bool snail_d_GetB(Snail &z);
-bool snail_d_distance(Snail &z);
-bool snail_d_print(Snail &z);
-bool snail_d_Type_of_Snail(Snail &z);
-bool snail_d_Square_snail(Snail &z);
-bool snail_d_Radius(Snail &z);
-bool snail_d_decart(Snail &z);
+void snail_d_SetA(Snail &z);
+void snail_d_SetB(Snail &z);
+void snail_d_GetA(Snail &z);
+void snail_d_GetB(Snail &z);
+void snail_d_distance(Snail &z);
+void snail_d_print(Snail &z);
+void snail_d_Type_of_Snail(Snail &z);
+void snail_d_Square_snail(Snail &z);
+void snail_d_Radius(Snail &z);
+void snail_d_decart(Snail &z);
 int vvodd(double &a);
-  int dialog(Snail &z);
-  
+int vvodm(int &a);
+int dialog(Snail &z);
